bf26d7a7162b4ee8042bde0f363bf8d7d7b4b1cf
DenisSangi <denissangi84@gmail.com>
2026-07-29 16:19:50 +0500

    P5-T1 is done

---
+++ src/main/java/reusableactions/LoginReusableActions.java
@@ -0,0 +1,20 @@
+package reusableactions;
+
+import com.codeborne.selenide.Selenide;
+import config.FrameworkConfig;
+import pages.HomePage;
+
+public class LoginReusableActions {
+
+    public HomePage loginAsDefaultUser() {
+        Selenide.open(FrameworkConfig.APP_URL);
+
+        return new HomePage().verifyPageIsOpen()
+                .clickSignupLoginLink()
+                .verifyPageIsOpen()
+                .setEmail(FrameworkConfig.APP_USER_EMAIL)
+                .setPassword(FrameworkConfig.APP_USER_PASSWORD)
+                .clickLoginButton()
+                .verifyLoggedUsername(FrameworkConfig.APP_USERNAME);
+    }
+}
