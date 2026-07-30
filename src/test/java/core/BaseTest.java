55b009dc1a55525a4393824a649665b3db2b624c
DenisSangi <denissangi84@gmail.com>
2026-07-30 12:29:35 +0500

    P6-T2 is done

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -2,10 +2,12 @@ package core;
 
 import com.codeborne.selenide.Selenide;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(ScreenshotOnFailureListener.class)
 public class BaseTest {
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
         Selenide.closeWebDriver();
     }
